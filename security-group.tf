d519a179a9f91132063227e5509d06af00459da1
Volodymyr Khomenko <45836705+volodymyr1213@users.noreply.github.com>
2019-04-20 16:55:42 -0500

    Create security-group.tf

---
+++ security-group.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.dev.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp
+    cidr_blocks = "0.0.0.0/0"
+    
+}
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
